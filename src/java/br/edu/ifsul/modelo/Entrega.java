8897e7cab32310cbb7b49e39d365f43bd235ebff
aluno <aluno@pc-90477.passofundo.ifsul.local>
2015-06-29 10:06:14 -0300

    model updated

--- src/java/br/edu/ifsul/modelo/Entrega.java
+++ src/java/br/edu/ifsul/modelo/Entrega.java
@@ -1,9 +1,57 @@
 package br.edu.ifsul.modelo;
 
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
 /**
- * Not Implemented
+ * 
  * @author Marcelo
  */
-public class Entrega {
+@Entity
+@Table(name = "entrega")
+public class Entrega implements Serializable{
+    
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(name = "seq_entrega", sequenceName = "seq_entrega_id", 
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_entrega")    
+    private Integer manifesto;
+    
+    @NotNull(message = "A quantia de volumes deve ser informada")
+    @Column(name = "volumes", length = 12, nullable = false, unique = true)      
+    Integer Volumes;
+
+    @NotNull(message = "O campo de verificação de entrega deve ser informado")
+    @Column(name = "foientregue", nullable = false)    
+    Boolean Foientregue;
+    
+    @NotNull(message = "A data da emissao deve ser informada")
+    @Column(name = "dataemissao", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Calendar DataEmissao;
+    
+    @NotNull(message = "A data da entrega deve ser informada")
+    @Column(name = "dataentrega", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Calendar DataEntrega;    
+    
+    @NotNull(message = "O valor deve ser informada")
+    @Column(name = "valor", nullable = false, columnDefinition = "decimal(12,2)")    
+    Double Valor;
+    
+    
+    
+    
     
 }
